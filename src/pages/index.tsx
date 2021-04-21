d67152dd588aad0ba2bba7f42fedfe7f002d5e90
Gustavo Fonseca <44001877+fonsecagustavo@users.noreply.github.com>
2021-04-21 14:48:15 -0300

    Segundo dia

---
+++ src/pages/index.tsx
@@ -0,0 +1,62 @@
+// SSG
+export default function Home(props) {
+  
+  return (
+    <div>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+    
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
+
+
+// SPA
+/** 
+import { useEffect } from "react"
+
+export default function Home() {
+  useEffect(()=> {
+    fetch('http://localhost:3333/episodes')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  },[]);
+
+  return (
+   <h1>Index</h1>
+  )
+}
+*/
+
+// SSR
+/** 
+export default function Home(props) {
+  
+  return (
+    <div>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+    
+  return {
+    props: {
+      episodes: data,
+    }
+  }
+}
+*/
